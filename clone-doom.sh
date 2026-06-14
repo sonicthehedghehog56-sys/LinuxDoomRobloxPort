6737362b97d898235205107e1456683c34104902
sonicthehedghehog56-sys <sonicthehedghehog56@gmail.com>
2026-06-13 21:58:18 -0500

    Add script to clone DOOM source files

---
+++ clone-doom.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Clone id-Software/DOOM repository into the current directory
+git clone https://github.com/id-Software/DOOM.git doom-source
+
+echo "✓ DOOM source files cloned successfully into 'doom-source' directory"
